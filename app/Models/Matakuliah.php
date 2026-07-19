63db602993a91e6dd879ba34b2cb1beeafdf9180
Sangskrt4 <ramadhancallstr@gmail.com>
2026-07-19 12:30:06 +0700

    Task 5: Sistem Nilai Mahasiswa selesai

--- app/Models/Matakuliah.php
+++ app/Models/Matakuliah.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Matakuliah extends Model
 {
-    //
+    use HasFactory;
+
+    // TAMBAHKAN BARIS INI UNTUK MEMPERBAIKI ERROR
+    protected $table = 'matakuliah'; 
+
+    public function nilai()
+    {
+        return $this->hasMany(Nilai::class);
+    }
 }
